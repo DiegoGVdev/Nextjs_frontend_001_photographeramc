e358d7d01770ab0993da7dcbc88fbe1d75277fee
diego galviz <dagalviz83@soy.sena.edu.co>
2023-09-25 21:16:45 -0500

    Feat/Home_brand text correction

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,28 +1,32 @@
+"use client";
+
 import Image from "next/image";
 import profile from "@/assets/profile/profile.png";
 
 function Banner() {
   return (
-    <section className="w-full mx-auto h-[1080px]">
-      <div className="container mx-auto flex justify-center p-4 m-4 ">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="font-bold text-3xl">
-            Anderson <span className="text-slate-500 text-5xl">MC</span>
-          </h1>
-          <div className="">
-            <span>I am a</span>
-            <h1 className="text-9xl">Photographer</h1>
+    <section className="w-full mx-auto md:h-[720px] h-auto flex justify-center items-center">
+      <div className="container mx-auto flex flex-col md:flex-row justify-center">
+        <div className="flex flex-col justify-center items-end p-2 mx-auto">
+          <h1 className="font-bold  text-slate-500 text-7xl">AMC</h1>
+          <div className="flex flex-col justify-center items-end">
+            <h1 className="text-3xl font-semibold">Photographer</h1>
+            <span className="">IN NYC</span>
           </div>
         </div>
 
         <div className="">
-          <Image width={400} height={400} src={profile} alt="profile-image" className="" />
+          <Image
+            width={400}
+            height={400}
+            src={profile}
+            alt="profile-image"
+            className="w-min-400 md:w-[600px]"
+          />
         </div>
-
       </div>
     </section>
   );
 }
 
 export default Banner;
-
